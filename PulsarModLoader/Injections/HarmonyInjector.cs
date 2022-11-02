b128d28195a3304cfaa20d2a0d697a81417f05f0
diddums <diddums@contact.me>
2022-11-02 01:43:03 -0700

    Updated logging to verbosly track harmony patches

--- PulsarModLoader/Injections/HarmonyInjector.cs
+++ PulsarModLoader/Injections/HarmonyInjector.cs
@@ -1,7 +1,10 @@
 ﻿using HarmonyLib;
+
 using System.Reflection;
 
 using PulsarModLoader.Utilities;
+
+using UnityEngine;
 
 namespace PulsarModLoader.Injections
 {
@@ -9,22 +12,45 @@ namespace PulsarModLoader.Injections
     {
         public static void InitializeHarmony()
         {
-            Logger.Info("Loading Harmony 6");
+            Utilities.Logger.Info("Loading Harmony 6");
             try
             {
                 Harmony.DEBUG = true;
-                Logger.Info($"Harmony.DEBUG {Harmony.DEBUG}");
+                Utilities.Logger.Info($"Harmony.DEBUG {Harmony.DEBUG}");
                 var harmony = new Harmony("wiki.pulsar.pml");
-                Logger.Info(harmony.Id);
+                Utilities.Logger.Info(harmony.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
-                Logger.Info("Patched methods ");
-                foreach (MethodBase m in harmony.GetPatchedMethods())
+                Utilities.Logger.Info("Patched methods ");
+                foreach (MethodBase m in Harmony.GetAllPatchedMethods())
                 {
-                    Logger.Info(m.FullDescription());
+                    Utilities.Logger.Info($"\t{m.FullDescription()}");
+
+                    HarmonyLib.Patches info = Harmony.GetPatchInfo(m);
+                    Utilities.Logger.Info($"\t\t{info}");
+                    Utilities.Logger.Info($"\t\t\tPrefixes: {info.Prefixes.Count}");
+                    foreach (Patch patch in info.Prefixes)
+                    {
+                        Utilities.Logger.Info($"\t\t\t\t{patch.PatchMethod.FullDescription()}");
+
+                    }
+                    Utilities.Logger.Info($"\t\t\tTranspilers: {info.Transpilers.Count}");
+                    foreach (Patch patch in info.Transpilers)
+                    {
+                        Utilities.Logger.Info($"\t\t\t\t{patch.PatchMethod.FullDescription()}");
+
+                    }
+                    Utilities.Logger.Info($"\t\t\tPostfixes: {info.Postfixes.Count}");
+                    foreach (Patch patch in info.Postfixes)
+                    {
+                        Utilities.Logger.Info($"\t\t\t\t{patch.PatchMethod.FullDescription()}");
+
+                    }
                 }
-            } catch (System.Exception e)
+
+            }
+            catch (System.Exception e)
             {
-                Logger.Info($"Failed to inject {e.Message} - {e}");
+                Utilities.Logger.Info($"Failed to inject {e.Message} - {e}");
             }
         }
     }
